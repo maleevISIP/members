bb7726b74cfb5a448549d3d66d2fa71c3f5dbc92
Egor Maleev <egor.maleev@list.ru>
2020-06-18 22:47:50 +0500

    egorka

---
+++ members/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace members
+{
+    class mem
+    {
+        public int idNumber;
+        private string sname;
+        public string name;
+        private string othestvo;
+        internal long cNumber;
+        internal int cTalk;
+        internal int inCTalk;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                mem memOne = new mem();
+
+                memOne.idNumber = Convert.ToInt32(Console.ReadLine());
+                memOne.name = Console.ReadLine();
+
+                Console.WriteLine(memOne.idNumber + " " + memOne.name);
+                Console.ReadKey();
+            }
+        }
+    }
+
